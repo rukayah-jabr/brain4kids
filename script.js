b0b747e13c9263cff23d3b31b8cb5e164af03c35
Aziz Ftaiti <azizftaiti19@gmail.com>
2024-01-26 20:49:23 +0100

    Start game Screen

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    alert('Play button clicked!');
+});
+
+document.getElementById('settingsButton').addEventListener('click', function() {
+    alert('Settings button clicked!');
+});
